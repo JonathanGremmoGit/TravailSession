d2285ed5eac66c88cdd9d0eb91a21367f37b21a0
BelderialSerperod <belderialserperod@gmail.com>
2023-11-14 13:34:29 -0500

    Ajout des classes Projet, Client et Employe

---
+++ TravailSession/TravailSession/Employe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSession
+{
+    internal class Employe
+    {
+        public string Matricule { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Email { get; set; }
+        public string Adresse { get; set; }
+        public DateTime DateEmbauche { get; set; }
+        public double TauxHoraire { get; set; }
+        public string PhotoIdentite { get; set; }
+        public string Statut { get; set; }
+    }
+}
---
+++ TravailSession/TravailSession/Projet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSession
+{
+    internal class Projet
+    {
+        public int NumeroProjet { get; set; }
+        public string Titre { get; set; }
+        public DateTime DateDebut { get; set; }
+        public string Description { get; set; }
+        public decimal Budget { get; set; }
+        public int NombreEmployesRequis { get; set; }
+        public decimal TotalSalairesPayer { get; set; }
+        public int ClientIdentifiant { get; set; }
+        public string Statut { get; set; }
+    }
+}
