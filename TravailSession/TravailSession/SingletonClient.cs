f42a793c8439a0b0e47fb444b37304e4bc316c8a
BelderialSerperod <belderialserperod@gmail.com>
2023-11-14 14:38:41 -0500

    début du SingletonClient et SingletonEmploye

---
+++ TravailSession/TravailSession/SingletonClient.cs
@@ -0,0 +1,90 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSession
+{
+    internal class SingletonClient
+    {
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=exointerfacemaison2;Uid=root;Pwd=;");
+
+        ObservableCollection<Client> liste;
+        static SingletonClient instance = null;
+
+        public SingletonClient()
+        {
+            liste = new ObservableCollection<Client>();
+        }
+
+        public static SingletonClient getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonClient();
+
+            return instance;
+        }
+
+        public ObservableCollection<Client> GetListeClients()
+        {
+            liste.Clear();
+
+            MySqlCommand commande = new MySqlCommand("p_get_liste_clients");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+            con.Open();
+
+            MySqlDataReader r = commande.ExecuteReader();
+
+            while (r.Read())
+            {
+                int identifiant = (int)r["identifiant"];
+                string nom = (string)r["nom"];
+                string adresse = (string)r["adresse"];
+                string numeroTelephone = (string)r["numeroTelephone"];
+                string email = (string)r["email"];
+
+                Client client = new Client
+                {
+                    Identifiant = identifiant,
+                    Nom = nom,
+                    Adresse = adresse,
+                    NumeroTelephone = numeroTelephone,
+                    Email = email
+                };
+
+                liste.Add(client);
+            }
+
+            r.Close();
+
+            con.Close();
+
+            return liste;
+        }
+
+        public void Ajouter(Client client)
+        {
+
+            MySqlCommand commande = new MySqlCommand("p_ajouter_client");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+            commande.Parameters.AddWithValue("p_Identifiant", $"{client.Identifiant}");
+            commande.Parameters.AddWithValue("p_Nom", $"{client.Nom}");
+            commande.Parameters.AddWithValue("p_Adresse", $"{client.Adresse}");
+            commande.Parameters.AddWithValue("p_NumeroTelephone", $"{client.NumeroTelephone}");
+            commande.Parameters.AddWithValue("p_Email", $"{client.Email}");
+
+            con.Open();
+            commande.Prepare();
+            int i = commande.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+    }
+}
---
+++ TravailSession/TravailSession/SingletonEmploye.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TravailSession
+{
+    internal class SingletonEmploye
+    {
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=exointerfacemaison2;Uid=root;Pwd=;");
+
+        ObservableCollection<Employe> liste;
+        static SingletonEmploye instance = null;
+
+        public SingletonEmploye()
+        {
+            liste = new ObservableCollection<Employe>();
+        }
+
+        public static SingletonEmploye getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonEmploye();
+
+            return instance;
+        }
+
+        public ObservableCollection<Employe> GetListeEmployes()
+        {
+            liste.Clear();
+
+            MySqlCommand commande = new MySqlCommand("p_get_liste_employes");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+            con.Open();
+
+            MySqlDataReader r = commande.ExecuteReader();
+
+            while (r.Read())
+            {
+                string matricule = (string)r["matricule"];
+                string nom = (string)r["nom"];
+                string prenom = (string)r["prenom"];
+                DateTime dateNaissance = (DateTime)r["dateNaissance"];
+                string email = (string)r["email"];
+                string adresse = (string)r["adresse"];
+                DateTime dateEmbauche = (DateTime)r["dateEmbauche"];
+                double tauxHoraire = (double)r["tauxHoraire"];
+                string photoIdentite = (string)r["photoIdentite"];
+                string statut = (string)r["statut"];
+
+                Employe employe = new Employe { Matricule = matricule, Nom = nom, Prenom = prenom,
+                    DateNaissance = dateNaissance, Email = email, Adresse = adresse, DateEmbauche = dateEmbauche,
+                    TauxHoraire = tauxHoraire, PhotoIdentite = photoIdentite, Statut = statut };
+
+                liste.Add(employe);
+            }
+
+            r.Close();
+
+            con.Close();
+
+            return liste;
+        }
+
+        public void Ajouter(Employe employe)
+        {
+
+            MySqlCommand commande = new MySqlCommand("p_ajouter_employe");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+            commande.Parameters.AddWithValue("p_Matricule", $"{employe.Matricule}");
+            commande.Parameters.AddWithValue("p_Nom", $"{employe.Nom}");
+            commande.Parameters.AddWithValue("p_Prenom", $"{employe.Prenom}");
+            commande.Parameters.AddWithValue("p_DateNaissance", $"{employe.DateNaissance}");
+            commande.Parameters.AddWithValue("p_Email", $"{employe.Email}");
+            commande.Parameters.AddWithValue("p_Adresse", $"{employe.Adresse}");
+            commande.Parameters.AddWithValue("p_DateEmbauche", $"{employe.DateEmbauche}");
+            commande.Parameters.AddWithValue("p_TauxHoraire", $"{employe.TauxHoraire}");
+            commande.Parameters.AddWithValue("p_PhotoIdentite", $"{employe.PhotoIdentite}");
+            commande.Parameters.AddWithValue("p_Statut", $"{employe.Statut}");
+            con.Open();
+            commande.Prepare();
+            int i = commande.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        public void Modifier(Employe employe, string attribut, string nouvelleValeur)
+        {
+
+            MySqlCommand commande = new MySqlCommand("p_modifier_employe");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+            commande.Parameters.AddWithValue("p_Matricule", $"{employe.Matricule}");
+            commande.Parameters.AddWithValue("p_attribut", $"{attribut}");
+            commande.Parameters.AddWithValue("p_attribut", $"{nouvelleValeur}");
+
+            con.Open();
+            commande.Prepare();
+            int i = commande.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        public void ChangerStatut(Employe employe)
+        {
+
+            MySqlCommand commande = new MySqlCommand("p_changer_statut");
+            commande.Connection = con;
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+            commande.Parameters.AddWithValue("p_Matricule", $"{employe.Matricule}");
+
+            con.Open();
+            commande.Prepare();
+            int i = commande.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+}
